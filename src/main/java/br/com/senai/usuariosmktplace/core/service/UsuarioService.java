a6455e8628c91da60c37c5a410bb461619460b95
Raul Nandi de Pieri <raul_pieri@estudante.sesisenai.org.br>
2023-08-24 20:11:56 -0300

    Início do desenvolvimento da atualização de usuário

--- src/main/java/br/com/senai/usuariosmktplace/core/service/UsuarioService.java
+++ src/main/java/br/com/senai/usuariosmktplace/core/service/UsuarioService.java
@@ -40,6 +40,34 @@ public class UsuarioService {
 		
 	}
 	
+	public Usuario atualizaPor(String login, String nomeCompleto, String senhaAntiga, String senhaNova) {
+		
+		Preconditions.checkArgument(!Strings.isNullOrEmpty(login), "O login é obrigatório para atualização");
+		Preconditions.checkArgument(!Strings.isNullOrEmpty(senhaAntiga), "A senha antiga é obrogatória para atualização");
+		this.validar(nomeCompleto, senhaNova);
+		
+		Usuario usuarioSalvo = this.dao.buscarPor(login);
+		Preconditions.checkNotNull(usuarioSalvo, "Não foi encontrado usuário vinculado ao login informado");
+		
+		String senhaAntigaCriptografada = gerarHashDa(senhaAntiga);
+		
+		boolean isSenhaValida = senhaAntigaCriptografada.equals(usuarioSalvo.getSenha());
+		Preconditions.checkArgument(isSenhaValida, "A senha antiga não confere");
+		
+		Preconditions.checkArgument(!senhaAntiga.equals(senhaNova), "A senha nova não pode ser igual a senha antiga");
+		
+		String senhaNovaCriptografada = gerarHashDa(senhaNova);
+		
+		Usuario usuarioAlterado = new Usuario(login, senhaNovaCriptografada, nomeCompleto);
+		
+		this.dao.alterar(usuarioAlterado);
+		
+		usuarioAlterado = this.dao.buscarPor(login);
+		
+		return usuarioAlterado;	
+		
+	}
+	
 	@SuppressWarnings("deprecation")
 	private void validar(String senha) {
 		
