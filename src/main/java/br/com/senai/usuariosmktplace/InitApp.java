6bc166217e484e4f17b19b71c3e469fc480c2f29
Raul Nandi de Pieri <raul_pieri@estudante.sesisenai.org.br>
2023-08-25 20:27:09 -0300

    Resolução de conflitos na branch

--- src/main/java/br/com/senai/usuariosmktplace/InitApp.java
+++ src/main/java/br/com/senai/usuariosmktplace/InitApp.java
@@ -1,24 +1,12 @@
 package br.com.senai.usuariosmktplace;
 
-<<<<<<< HEAD
-import br.com.senai.usuariosmktplace.core.domain.Usuario;
-=======
 import br.com.senai.usuariosmktplace.core.service.UsuarioService;
->>>>>>> feature/novo-usuario
 
 public class InitApp {
 
 	public static void main(String[] args) {
 		
-<<<<<<< HEAD
-		Usuario usuario = new Usuario("jose.silva", "José da Silva", "jose2023");
-		
-		System.out.println(usuario.getLogin());
-=======
 		UsuarioService service = new UsuarioService();
-		
-		service.criarPor("Renan Alberto Souza", "AHAHA123456");
->>>>>>> feature/novo-usuario
 		
 	}
 
